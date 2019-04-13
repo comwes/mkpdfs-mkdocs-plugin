22a1719d837d5bcd6ae3ea7d90e526f6fcbd0e55
Gerry Ntabuhashe <gntabuhashe@parlementfrancophone.brussels>
2019-04-13 13:00:38 +0200

    Update packages, error meet with cairo on documentation

--- .docs.sh
+++ .docs.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" -a "$GEN_
   git config --global user.name "${GH_NAME}"
   git config --global user.email "${GH_EMAIL}"
   git remote set-url origin ${REMOTE}
-
+  sudo apt-get update
   sudo apt-get install -y build-essential libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 
   # Build documentation with overrides and publish to GitHub pages
