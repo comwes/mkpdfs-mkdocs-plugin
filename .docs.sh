465e4b6d4b9197726904809d71ecdc88c6425fda
Gerry Ntabuhashe <gntabuhashe@parlementfrancophone.brussels>
2019-04-13 12:54:51 +0200

    Update source-list to install the right packages

--- .docs.sh
+++ .docs.sh
@@ -9,7 +9,7 @@ if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" -a "$GEN_
   git config --global user.email "${GH_EMAIL}"
   git remote set-url origin ${REMOTE}
 
-  sudo apt-get install -y build-essential python3-dev python3-pip python3-setuptools python3-wheel python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
+  sudo apt-get install -y build-essential libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info
 
   # Build documentation with overrides and publish to GitHub pages
   mkdocs gh-deploy --force
