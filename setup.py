c8450ce2489494c7e55b1b223a625ef39a8432d2
jurgenwigg <jurgenwigg@protonmail.com>
2023-10-03 19:31:06 +0000

    refactor: updated setup.py

--- setup.py
+++ setup.py
@@ -1,59 +1,49 @@
 from setuptools import setup, find_packages
-from os import path
-from io import open
+from pathlib import Path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
+long_description = (Path(__file__).parent / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='mkpdfs2-mkdocs',
-    version='1.0.1',
-    url='https://mkpdfs.comwes.eu',
-    license='GPLv3',
-    author='Comwes',
-    author_email='contact@comwes.eu',
-    maintainer='jurgenwigg',
-    maintainer_email='jurgenwigg@protonmail.com',
-    description='Allows the generation of the PDF version of your MkDocs documentation.',
+    name="mkpdfs2-mkdocs",
+    version="1.0.1",
+    url="https://github.com/jurgenwigg/mkpdfs2-mkdocs-plugin",
+    license="GPLv3",
+    author="Comwes",
+    author_email="contact@comwes.eu",
+    maintainer="jurgenwigg",
+    maintainer_email="jurgenwigg@protonmail.com",
+    description="Allows the generation of the PDF version of your MkDocs documentation.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    python_requires='>=3.4',
+    long_description_content_type="text/markdown",
+    python_requires=">=3.4",
     include_package_data=True,
-    install_requires=[
-        'mkdocs>=0.17',
-        'weasyprint>=0.44',
-        'beautifulsoup4>=4.6.3'
-    ],
+    install_requires=["mkdocs>=0.17", "weasyprint>=0.44", "beautifulsoup4>=4.6.3"],
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/jurgenwigg/mkpdfs2-mkdocs-plugin/issues',
-        'Source': 'https://github.com/jurgenwigg/mkpdfs2-mkdocs-plugin',
+        "Bug Reports": "https://github.com/jurgenwigg/mkpdfs2-mkdocs-plugin/issues",
+        "Source": "https://github.com/jurgenwigg/mkpdfs2-mkdocs-plugin",
     },
-    keywords='mkdocs documentation pdf export weasyprint markdown plugin',
+    keywords="mkdocs documentation pdf export weasyprint markdown plugin",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Topic :: Documentation',
-        'Topic :: Printing',
-        'Programming Language :: Other',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Documentation",
+        "Topic :: Printing",
+        "Programming Language :: Other",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     packages=find_packages(),
-
     entry_points={
-        'mkdocs.plugins': [
-            'mkpdfs = mkpdfs_mkdocs:Mkpdfs',
+        "mkdocs.plugins": [
+            "mkpdfs = mkpdfs_mkdocs:Mkpdfs",
         ]
     },
     zip_safe=False,
